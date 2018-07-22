de65a57db4901dc7ca6c12ecdd2c2a9cc8e2b81a
SunandaMukherjee23 <41181693+SunandaMukherjee23@users.noreply.github.com>
2018-07-22 13:23:13 +0530

    selectComplaint method has been modified on complaint di view component

---
+++ src/app/modules/shared/components/process-flow/process-flow-status-details.model.ts
@@ -0,0 +1,20 @@
+import { ROUTE_PATHS } from '../../../router/router-paths';
+export class ProcessFlowStatusDetailsModel {
+   
+    private _statusDetails: any[] = [
+        {statusId: 10, statusName: 'Registration', viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint},
+        {statusId: 40, statusName: 'Investigation Report', viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint},
+        {statusId: 50, statusName: 'RCA',viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint},
+        {statusId: 60, statusName: 'CA', viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint},
+        {statusId: 70, statusName: 'PA', viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint},
+        {statusId: 80, statusName: 'Closed', viewRoute: '#'+ROUTE_PATHS.RouteComplainDIViewDetails+'View', editRoute: '#'+ROUTE_PATHS.RouteModifyComplaint}
+    ]
+
+    public get statusDetails(): any[]{
+        return this._statusDetails;
+    }
+
+    public set statusDetails(statusDetails: any[]) {
+        this._statusDetails = statusDetails;
+      }
+}
