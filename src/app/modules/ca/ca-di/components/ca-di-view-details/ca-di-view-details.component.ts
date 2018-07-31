1311368d5a3451a2c2a8ca6122482b90bbaa3951
SunandaMukherjee23 <41181693+SunandaMukherjee23@users.noreply.github.com>
2018-07-31 18:12:35 +0530

    ca-di-31.07.18

--- src/app/modules/ca/ca-di/components/ca-di-view-details/ca-di-view-details.component.ts
+++ src/app/modules/ca/ca-di/components/ca-di-view-details/ca-di-view-details.component.ts
@@ -3,12 +3,12 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';//to get route param
 import { DatePipe } from '@angular/common';
-import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ROUTE_PATHS } from '../../../../router/router-paths';
 import { LocalStorageService } from "../../../../shared/services/local-storage.service";
 import { SessionErrorService } from "../../../../shared/services/session-error.service";
 import { CADIService } from "../../services/ca-di-add-edit.service";
-import { NgbdModalComponent } from '../../../../widget/modal/components/modal-component';
+// import { NgbdModalComponent } from '../../../../widget/modal/components/modal-component';
 
 
 @Component({
@@ -19,49 +19,37 @@ import { NgbdModalComponent } from '../../../../widget/modal/components/modal-co
 })
 export class CADIViewDetailsComponent implements OnInit {
 
-  // public fileList: FileList;
-  public title: string = "CA";//to show titlee on html page
-  public complaintReferenceNo: string;//to get complaint reference no from route param
   public caDIAddEditFormGroup: FormGroup;
+  public title: string = "CA";//to show titlee on html page
+  public routeParam: any ={
+    complaintReferenceNo:'',//to get complaint reference no from route param
+    complaintStatus:  ''//to fetch complaint status from route
+  }; 
   //for busy spinner
-  public busySpinner: any = {
-    compRefDetBusy: true,
-    submitBusy: false,//for submit spinner
-    busy: true
-  };
-  public currentDate: string;//for sysdate
-  public caAddEditDate: string;//close date
-  public selectedComplaintReferenceDetails: any = {};//to get selected complaint values  
-  public caAddEditDetails: string = "";//text area value for rca details
-
-  public complaintStatus: string = "";//to fetch complaint status from route
-
+  public busySpinner: boolean = false;
   //for error msg
-  public errMsgShowFlag: boolean = false;//to show the error msg div
-  public errorMsg: string;//to store the error msg
-
+  public errorMsgObj: any = {
+    errorMsg: '',
+    errMsgShowFlag: false
+  };
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private activatedroute: ActivatedRoute,
     private datePipe: DatePipe,//for date
-    private modalService: NgbModal,
     private localStorageService: LocalStorageService,
     private sessionErrorService: SessionErrorService,
     private cADIService: CADIService
   ) {
-
+    this.buildForm();//build form
   }//end of constructor
 
   ngOnInit(): void {
     console.log("onInit of CADIViewDetailsComponent..");
-    this.buildForm();//build form
     this.getRouteParam();//calling method to get route param 
     this.getSystemDate();//method to get system date
-
-    this.busySpinner.busy = false;
   }//end of on init
-
+  
   //a method named buildform for creating the caDIAddEditFormGroup and its formControl
   private buildForm(): void {
     this.caDIAddEditFormGroup = this.formBuilder.group({
@@ -78,45 +66,21 @@ export class CADIViewDetailsComponent implements OnInit {
   private getRouteParam() {
     let routeSubscription: Subscription;
     routeSubscription = this.activatedroute.params.subscribe(params => {
-      this.complaintReferenceNo = params.complaintReferenceNo ? params.complaintReferenceNo : 'DI000009';
-      this.complaintStatus = params.complaintStatus ? params.complaintStatus : '';
-    
+      this.routeParam.complaintReferenceNo = params.complaintReferenceNo ? params.complaintReferenceNo : '';
+      this.routeParam.complaintStatus = params.complaintStatus ? params.complaintStatus : '';    
     });
-    console.log("complaintReferenceNo for ca di view: ", this.complaintReferenceNo);
-    console.log("this.complaintStatus for ca di view::",this.complaintStatus);
+    console.log("complaintReferenceNo for ca di view: ", this.routeParam.complaintReferenceNo);
+    console.log("this.complaintStatus for ca di view::",this.routeParam.complaintStatus);
+    this.caDIAddEditFormGroup.controls['complaintReferenceNo'].setValue(this.routeParam.complaintReferenceNo);
   }//end of method
 
   //method to get system date
   private getSystemDate() {
     //formatting the current date
     let date = new Date();
-    this.currentDate = this.datePipe.transform(date, 'yyyy-MM-dd');
-    this.caDIAddEditFormGroup.controls["caAddEditDate"].setValue(this.currentDate);
-    // this.caAddEditDate = this.datePipe.transform(this.currentDate, 'yyyy-MM-dd');
-    this.caAddEditDate = this.datePipe.transform(this.currentDate, 'yyyy-MM-dd');
-    console.log("  rca::: this.caAddEditDate   ", this.caAddEditDate);
+    let currentDate: string = this.datePipe.transform(date, 'yyyy-MM-dd');
+    this.caDIAddEditFormGroup.controls["caAddEditDate"].setValue(currentDate);
   }//end of method
-
-  //to load the spinner
-  private updateBusySpinner() {
-    if (this.busySpinner.compRefDetBusy || this.busySpinner.submitBusy) {
-      this.busySpinner.busy = true;
-    } else if (this.busySpinner.compRefDetBusy == false || this.busySpinner.submitBusy == false) {
-      this.busySpinner.busy = false;
-    }
-  }//end of busy spinner method
-
-  //onOpenModal for opening modal from modalService
-  private onOpenModal(complaintRefNo,modalMsg) {
-    const modalRef = this.modalService.open(NgbdModalComponent);
-    modalRef.componentInstance.modalTitle = 'Information';
-    modalRef.componentInstance.modalMessage = modalMsg;
-      // this.complaintReferenceNo ?
-      //   "Complaint Reference Number(DI) " + complaintRefNo + " updated successfully."
-      //   : "Complaint Reference Number(DI) " + complaintRefNo + " created successfully.";
-  }
-  //end of method onOpenModal
-
 
   //for clicking cancel button this method will be invoked
   public onCancel(): void {
