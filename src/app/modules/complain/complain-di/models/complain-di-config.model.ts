1a50061a14c5dc123cf89b2a774e387a0ae7c4ee
SunandaMukherjee23 <41181693+SunandaMukherjee23@users.noreply.github.com>
2018-08-16 20:15:52 +0530

    some features added in many pages..

--- src/app/modules/complain/complain-di/models/complain-di-config.model.ts
+++ src/app/modules/complain/complain-di/models/complain-di-config.model.ts
@@ -15,25 +15,21 @@ export class ComplaintDIConfigModel {
         this._prevComplainHeader = prevComplainHeader;
     }
 
-    private _invItemGridHeader: any[] = [
-        { headerKey: 'complaintReferenceNo', headerDesc: 'Complaint Ref. No', headerClass: 'header-style-first visible-style', dataClass: 'td-style-first visible-style' },
-        { headerKey: 'complaintDetailsAutoId', headerDesc: 'Complaint Ref. Auto Id', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'invoiceNo', headerDesc: 'Official Document No', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'invoiceDate', headerDesc: 'Official Document Date', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'itemNo', headerDesc: 'Item Code', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'itemName', headerDesc: 'Item Description', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'invoiceQtyInMtrs', headerDesc: 'Invoice Qty(in Mtrs)', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'complaintQtyInMtrs', headerDesc: 'Complain Qty(in Mtrs)', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style', inputType:' <input type="text"></input>' },
-        { headerKey: 'customerName', headerDesc: 'Customer Name', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'customerCode', headerDesc: 'Customer Code', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'projectName', headerDesc: 'Project Name', headerClass: 'header-style visible-style', dataClass: 'td-style visible-style' },
-        { headerKey: 'projectLocation', headerDesc: 'Project Location', headerClass: 'header-style-last visible-style', dataClass: 'td-style-last visible-style' }
+    private _itemGridHeader: any[] = [
+        { headerKey: 'invoiceNo', headerDesc: 'Official Document No', headerClass: 'header-style', dataClass: 'td-style-first' },
+        { headerKey: 'itemNo', headerDesc: 'Item Code', headerClass: 'header-style', dataClass: 'td-style' },
+        { headerKey: 'invoiceDate', headerDesc: 'Official Document Date', headerClass: 'header-style', dataClass: 'td-style' },
+        { headerKey: 'itemName', headerDesc: 'Item Description', headerClass: 'header-style', dataClass: 'td-style' },
+        { headerKey: 'invoiceQtyInMtrs', headerDesc: 'Invoice Qty(in Mtrs)', headerClass: 'header-style', dataClass: 'td-style' },
+        { headerKey: 'complaintQtyInMtrs', headerDesc: 'Complain Qty(in Mtrs)', headerClass: 'header-style ', dataClass: 'td-style'},
+        { headerKey: 'projectName', headerDesc: 'Project Name', headerClass: 'header-style', dataClass: 'td-style' },
+        { headerKey: 'projectLocation', headerDesc: 'Project Location', headerClass: 'header-style-last', dataClass: 'td-style-last' }
     ]
 
-    get invItemGridHeader(): any[] {
-        return this._invItemGridHeader;
+    get itemGridHeader(): any[] {
+        return this._itemGridHeader;
     }
-    set invItemGridHeader(invItemGridHeader: any[]) {
-        this._invItemGridHeader = invItemGridHeader;
+    set itemGridHeader(itemGridHeader: any[]) {
+        this._itemGridHeader = itemGridHeader;
     }
 }
