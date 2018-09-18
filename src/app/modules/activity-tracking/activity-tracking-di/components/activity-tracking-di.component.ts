b4c813f12c6799dc0ae95659f31bb29b48a04313
susmitaseal <41181693+susmitaseal@users.noreply.github.com>
2018-09-18 18:25:38 +0530

    comp-status style-changes in comp-status component n a json is chenged in activity-tracking-->18-09-18

--- src/app/modules/activity-tracking/activity-tracking-di/components/activity-tracking-di.component.ts
+++ src/app/modules/activity-tracking/activity-tracking-di/components/activity-tracking-di.component.ts
@@ -9,13 +9,13 @@ import { Component } from '@angular/core';
 
 export class ActivityTrackingDIComponent {
     public testjson: any[] = [
-        {compNo:'DI000001',comSetStatus:'On',checked:true},
-        {compNo:'DI000002',comSetStatus:'Off',checked:false},
-        {compNo:'DI000003',comSetStatus:'On',checked:true},
-        {compNo:'DI000004',comSetStatus:'On',checked:true},
-        {compNo:'DI000005',comSetStatus:'Off',checked:false},
-        {compNo:'DI000006',comSetStatus:'On',checked:true},
-        {compNo:'DI000007',comSetStatus:'On',checked:true},
-        {compNo:'DI000008',comSetStatus:'On',checked:true}
+        {compNo:'DI000001',comSetStatus:'On',checked:true,compstatus:10},
+        {compNo:'DI000002',comSetStatus:'Off',checked:false,compstatus:40},
+        {compNo:'DI000003',comSetStatus:'On',checked:true,compstatus:50},
+        {compNo:'DI000004',comSetStatus:'On',checked:true,compstatus:60},
+        {compNo:'DI000005',comSetStatus:'Off',checked:false,compstatus:70},
+        {compNo:'DI000006',comSetStatus:'On',checked:true,compstatus:80},
+        {compNo:'DI000007',comSetStatus:'On',checked:true,compstatus:50},
+        {compNo:'DI000008',comSetStatus:'On',checked:true,compstatus:10}
     ];
 }
--- src/app/modules/widget/comp-status-structure/components/comp-status-structure.component.ts
+++ src/app/modules/widget/comp-status-structure/components/comp-status-structure.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { CompStatusStructureModel } from '../models/comp-status-structure.model';
-
+export interface IcompStatusStructure {
+  flowName: string, flowMsg: string, flowDate: string, headerClass?: string, footerClass?: string
+}
 @Component({
-    selector: 'ispl-comp-status-structure',
-    templateUrl: 'comp-status-structure.component.html',
-    styleUrls: ['comp-status-structure.component.css']
-  })
+  selector: 'ispl-comp-status-structure',
+  templateUrl: 'comp-status-structure.component.html',
+  styleUrls: ['comp-status-structure.component.css']
+})
 
-  export class CompStatusStructureComponent implements OnInit{
-    public compStatusStructure: any[] = [];
-    
-    constructor(){ 
-      this.compStatusStructure = new CompStatusStructureModel().compStatusStructureModel;
+export class CompStatusStructureComponent implements OnInit, OnChanges {
+  @Input() compStatus;
+  public compStatusStructure: any[] = [];
+  public compStatusStructureList: IcompStatusStructure[] = [];
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+  ngOnChanges() {
+    this.compStatusStructure = new CompStatusStructureModel().compStatusStructureModel;
+    console.log(this.compStatusStructure);
+
+    this.compStatus;
+    this.compStatusStructureList = this.constructCompStatusStructureFlow();
+  }
+
+  private constructCompStatusStructureFlow() {
+    // let stopFlag: boolean = false;
+    let pageFlowData: any[] = [];
+
+    for (let process of this.compStatusStructure) {
+      let compStatusStruc: IcompStatusStructure = {
+        flowName: process.shortName,
+        flowMsg: process.messege,
+        flowDate: process.date
+      };
+      if (process.status <= this.compStatus) {
+        compStatusStruc.headerClass = 'active-header-style';
+        compStatusStruc.footerClass = 'active-footer-style';
+      }
+      else if (process.statusId != this.compStatus) {
+        compStatusStruc.headerClass = 'inactive-header-style';
+        compStatusStruc.footerClass = 'inactive-footer-style';
+
+      }
+      pageFlowData.push(compStatusStruc);
     }
 
-    ngOnInit(): void {
+    return pageFlowData;
+  }
 
-    }
 }
