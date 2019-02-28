9dc127fb27f2157a789bc3bba43b95a7678dc3b8
susmitaseal <41181693+susmitaseal@users.noreply.github.com>
2019-02-28 15:15:29 +0530

    dashboard-date field name has been changed

--- src/app/modules/dashboard/components/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard.component.ts
@@ -118,7 +118,7 @@ export class DashboardComponent implements OnInit {
       sortData: '',
       orderType: '',
        //filter: "CMPLNT_LOGD_ON BETWEEN \'" + this.fromDate + " 00:00:00\' AND \'" + this.toDate + " 23:59:59\'",
-       filter: "CMPLNT_LOGD_ON BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
+       filter: "CMPLNT_LOGD_ON_ACTUAL BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
        + " 00:00:00\',103) AND CONVERT(datetime,\'" + this.datePipe.transform(this.toDate, 'dd/MM/yyyy') + " 23:59:59\',103)",
 
       fromDate: this.fromDate,
@@ -128,7 +128,7 @@ export class DashboardComponent implements OnInit {
     //set filter to get di total complaint  
     let diTilesFilter: any = {
       //filter: "CMPLNT_LOGD_ON BETWEEN \'" + this.fromDate + " 00:00:00\' AND \'" + this.toDate + " 23:59:59\'",
-      filter: "CMPLNT_LOGD_ON BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
+      filter: "CMPLNT_LOGD_ON_ACTUAL BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
       + " 00:00:00\',103) AND CONVERT(datetime,\'" + this.datePipe.transform(this.toDate, 'dd/MM/yyyy') + " 23:59:59\',103)",
       
       fromDate: this.fromDate,
@@ -185,7 +185,7 @@ export class DashboardComponent implements OnInit {
     tilesFilter.filter = this.localStorageService.appSettings.activityIdFieldName + " = "
       + this.localStorageService.appSettings.closeComplaintActivityId
       //+ " AND CMPLNT_LOGD_ON BETWEEN '" + this.fromDate + " 00:00:00' AND '" + this.toDate + " 23:59:59'"
-      + " AND CMPLNT_LOGD_ON BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
+      + " AND CMPLNT_LOGD_ON_ACTUAL BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
       + " 00:00:00\',103) AND CONVERT(datetime,\'" + this.datePipe.transform(this.toDate, 'dd/MM/yyyy') + " 23:59:59\',103)";
 
     tilesFilter.fileActivityId = this.localStorageService.appSettings.closeComplaintActivityId;
@@ -194,7 +194,7 @@ export class DashboardComponent implements OnInit {
     let diTilesFilter: any = {
       filter: this.localStorageService.appSettings.lastActivityIdFieldName + '=' + 80//close activity id
         //+ " AND CMPLNT_LOGD_ON BETWEEN '" + this.fromDate + " 00:00:00' AND '" + this.toDate + " 23:59:59'",
-        + " AND CMPLNT_LOGD_ON BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
+        + " AND CMPLNT_LOGD_ON_ACTUAL BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
         + " 00:00:00\',103) AND CONVERT(datetime,\'" + this.datePipe.transform(this.toDate, 'dd/MM/yyyy') + " 23:59:59\',103)",
       fromDate: this.fromDate,
       toDate: this.toDate
@@ -248,7 +248,7 @@ export class DashboardComponent implements OnInit {
       + " AND " + this.localStorageService.appSettings.activityIdFieldName + " < "
       + this.localStorageService.appSettings.closeComplaintActivityId
       //+ " AND CMPLNT_LOGD_ON BETWEEN \'" + this.fromDate + " 00:00:00\' AND \'" + this.toDate + " 23:59:59\'";
-      + " AND CMPLNT_LOGD_ON BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
+      + " AND CMPLNT_LOGD_ON_ACTUAL BETWEEN CONVERT(datetime,\'" + this.datePipe.transform(this.fromDate, 'dd/MM/yyyy')
       + " 00:00:00\',103) AND CONVERT(datetime,\'" + this.datePipe.transform(this.toDate, 'dd/MM/yyyy') + " 23:59:59\',103)";
 
     tilesFilter.fileActivityId = this.localStorageService.appSettings.pendingComplaintActivityId;
